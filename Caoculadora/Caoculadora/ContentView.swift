d77465fde082f225cf7b60bd4ffbee3d8f4512a5
Keine <keine00@gmail.com>
2024-05-08 10:04:34 -0300

    Adiciona text field

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var years: Int?
+    @State var months: Int?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "pawprint")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Qual a idade do seu cão?")
+            
+            Text("Anos")
+            TextField("Quantos anos completos tem seu cão?",
+                      value: $years,
+                      format: .number)
+            Text("Meses")
+            TextField("Quantos meses tem seu cão?", value: $months, format: .number)
+            Text("Porte")
         }
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
         .padding()
     }
 }
